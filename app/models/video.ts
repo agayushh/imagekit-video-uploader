d432b0435521dce9f93e7c3e03680df6c7185d09
agayushh <agayush088@gmail.com>
2025-08-15 15:31:09 +0530

    Refactor video model to use InferSchemaType and enhance schema structure

--- app/models/video.ts
+++ app/models/video.ts
@@ -1,41 +1,47 @@
-import mongoose, { Schema, model, models } from "mongoose";
+import mongoose, { Schema, model, models, InferSchemaType } from "mongoose";
 
 export const VIDEO_DIMENSIONS = {
   width: 1080,
   height: 1920,
-} as const;
+} as const; //we are doing this to fix the values of video dimension because phele wale cosnt se values override hoskti hai
 
-export interface IVideo {
-  _id?: mongoose.Types.ObjectId;
-  title: string;
-  description: string;
-  videoUrl: string;
-  thumbnailUrl: string;
-  controls?: boolean;
-  transformations?: {
-    height: number;
-    width: number;
-    quality?: number;
-  };
-}
-
-const videoSchema = new Schema<IVideo>(
+const videoSchema = new Schema(
   {
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    videoUrl: { type: String, required: true },
-    thumbnailUrl: { type: String, required: true },
-    controls: { type: Boolean, default: true },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    thumbnailUrl: {
+      type: String,
+      required: false,
+    },
+    id: {
+      type: mongoose.Types.ObjectId,
+      required: false,
+    },
+    controls: {
+      type: Boolean,
+    },
     transformations: {
-      height: { type: Number, default: VIDEO_DIMENSIONS.height },
-      width: { type: Number, default: VIDEO_DIMENSIONS.width },
-      quality: { type: Number, min: 1, max: 1 },
+      type: {
+        width: { type: Number, required: true },
+        height: { type: Number, required: true },
+        quality: { type: Number, required: false },
+      },
+      required: true,
     },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-const Video = models?.Video || model<IVideo>("Video", videoSchema);
-export default Video;
+type videoType = InferSchemaType<typeof videoSchema>;
+
+const Video = models?.Video || model<videoType>("Video", videoSchema);
